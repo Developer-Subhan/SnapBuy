43328cf058d34d3fbc1df78e26bd20cf7c560d38
Developer-Subhan <mirzasubhan1010@gmail.com>
2026-01-03 14:55:50 +0500

    initial commit

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const passport = require("passport");
 const helmet = require("helmet");
 const User = require("./models/User");
 const LocalStrategy = require("passport-local");
-const { MongoStore } = require("connect-mongo");
+const MongoStore = require("connect-mongo");
 
 const productRoutes = require("./routes/product");
 const orderRoutes = require("./routes/order");
@@ -39,18 +39,15 @@ app.use(async (req, res, next) => {
 
 const whitelist = ["https://snapbuy-main.vercel.app", "http://localhost:5173"];
 
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
-  if (req.method === "OPTIONS") return next();
-  if (!origin || !whitelist.includes(origin)) {
-    return res.status(403).json({ message: "Access Denied: Unauthorized Request Source" });
-  }
-  next();
-});
-
 app.use(cors({
-  origin: whitelist,
-  credentials: false,
+  origin: function (origin, callback) {
+    if (!origin || whitelist.some(url => origin.startsWith(url))) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
 }));
 
 app.use(express.json());
@@ -76,7 +73,7 @@ const sessionOption = {
     maxAge: fourteenDaysInSeconds * 1000,
     httpOnly: true,
     secure: true,
-    sameSite: "lax",
+    sameSite: "none",
   }
 };
 
